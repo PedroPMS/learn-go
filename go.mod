3efa2535238aea6ac520a7dc09e2d874ee89f369
Pedro Silva <p.paulo0512@gmail.com>
2024-10-08 20:46:44 -0300

    iteration

---
+++ go.mod
@@ -0,0 +1,3 @@
+module learn-go-with-tets
+
+go 1.23.2
---
+++ iteration/repeat_test.go
@@ -0,0 +1,39 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	t.Run("repeat 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("repeat 3 times", func(t *testing.T) {
+		repeated := Repeat("a", 3)
+		expected := "aaa"
+		assertCorrectMessage(t, repeated, expected)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
